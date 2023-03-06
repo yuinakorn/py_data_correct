156d331056f7c337df02670c2d4fa859860f1fee
Yui Nakorn <yuinakorn@hotmail.com>
2023-03-06 22:24:36 +0700

    setting.py debug false

--- py_data_correct/settings.py
+++ py_data_correct/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SETTING_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config['WEB_IP'], config['WEB_NAME']]
 
