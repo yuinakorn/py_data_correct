06100273c9f64b2d893225d4603e57fde665d5a0
yuinakorn <yuinakorn@hotmail.com>
2024-05-12 19:29:06 +0700

    update mode setting.

--- py_data_correct/settings.py
+++ py_data_correct/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SETTING_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['MODE']
+DEBUG = config['SETTING_DEBUG']
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config['WEB_LOCAL_IP'], config['WEB_IP'], config['WEB_NAME']]
 
